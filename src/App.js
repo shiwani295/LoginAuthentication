36fedab13bec03f3660cb4619a4b688fcf900614
shiwani295 <shiwanitiwarishiwani@gmail.com>
2023-03-19 15:21:13 +0530

    worked on /profile will not seen directly its depend on user login or logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCxt = useContext(AuthContext);
   return (
     <Layout>
       <Switch>
         <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
+        {!authCxt.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
         <Route path="/profile">
-          <UserProfile />
+          {authCxt.isLoggedIn && <UserProfile />}
+          {!authCxt.isLoggedIn && <Redirect to="/auth" />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
